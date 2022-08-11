41399cdf13d49b15f3a4f8f90708d771aa734777
ruizdani301 <ruizdani301@gmail.com>
2022-08-11 14:13:31 -0500

    se agrega PRIMARY-KEY

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,9 +2,9 @@
 -- Write a SQL script that creates a table users following these requirements:
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255),
     country ENUM ('US', 'CO', 'TN') DEFAULT 'US'
-    PRIMARY KEY (id)
+    
 )
